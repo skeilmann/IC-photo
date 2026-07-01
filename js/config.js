e96674fe19b64c066a5cbd297b279870cabc310e
George Cazacu <113353753+skeilmann@users.noreply.github.com>
2026-07-01 22:25:59 +0200

    added sensitive data

--- js/config.js
+++ js/config.js
@@ -6,16 +6,16 @@
 window.IC_CONFIG = {
   // 1) Google Cloud API key (restricted to the Drive API + your site's domain).
   //    Used only to READ the public shared folder for the gallery.
-  API_KEY: "YOUR_API_KEY",
+  API_KEY: "AIzaSyA3Rlc2Whv8iGFxVAV2POvcZMkIlocTHVw",
 
   // 2) OAuth 2.0 Client ID (type: Web application) from the same project.
   //    Used for "Sign in with Google" so friends can upload.
-  CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+  CLIENT_ID: "412077965645-splfu4dkld37280en1v5l5qr7er4f05v.apps.googleusercontent.com",
 
   // 3) The ID of the shared Google Drive folder that holds the photos.
   //    It's the last part of the folder URL:
   //    https://drive.google.com/drive/folders/<THIS_PART>
-  FOLDER_ID: "YOUR_FOLDER_ID",
+  FOLDER_ID: "https://drive.google.com/drive/folders/14OS_wOhBpwwRQtuworJfFxf8V7lkoU0I?usp=sharing",
 
   // Optional: shown as the site/album name in a few places.
   EVENT_NAME: "Stockholm Convention 2026",
